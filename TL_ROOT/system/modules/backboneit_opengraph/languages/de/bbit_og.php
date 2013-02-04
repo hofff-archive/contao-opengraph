56cd9cd6dc002f9c97c386d3619fc20ca43cfe97
Oliver Hoff <oliver@hofff.com>
2013-02-04 03:10:41 +0000

    formatting paranoia

--- TL_ROOT/system/modules/backboneit_opengraph/languages/de/bbit_og.php
+++ TL_ROOT/system/modules/backboneit_opengraph/languages/de/bbit_og.php
@@ -1,21 +1,23 @@
 <?php
 
 $GLOBALS['TL_LANG']['bbit_og']['types']['general']				= 'Allgemein';
-$GLOBALS['TL_LANG']['bbit_og']['types']['custom']				= 'Eigener Typ';
 $GLOBALS['TL_LANG']['bbit_og']['types']['website']				= 'Webseite';
 $GLOBALS['TL_LANG']['bbit_og']['types']['article']				= 'Artikel / Beitrag / Eintrag';
 $GLOBALS['TL_LANG']['bbit_og']['types']['profile']				= 'Profil';
+
 $GLOBALS['TL_LANG']['bbit_og']['types']['book']					= 'Buch';
 $GLOBALS['TL_LANG']['bbit_og']['types']['music']				= 'Musik';
 $GLOBALS['TL_LANG']['bbit_og']['types']['music.song']			= 'Lied';
 $GLOBALS['TL_LANG']['bbit_og']['types']['music.album']			= 'Album';
 $GLOBALS['TL_LANG']['bbit_og']['types']['music.playlist']		= 'Abspielliste';
 $GLOBALS['TL_LANG']['bbit_og']['types']['music.radio_station']	= 'Radiosender';
+
 $GLOBALS['TL_LANG']['bbit_og']['types']['video']				= 'Video';
 $GLOBALS['TL_LANG']['bbit_og']['types']['video.movie']			= 'Kinofilm';
 $GLOBALS['TL_LANG']['bbit_og']['types']['video.episode']		= 'Episode';
 $GLOBALS['TL_LANG']['bbit_og']['types']['video.tv_show']		= 'TV-Show';
 $GLOBALS['TL_LANG']['bbit_og']['types']['video.other']			= 'Sonstiges';
+$GLOBALS['TL_LANG']['bbit_og']['types']['custom']				= 'Eigener Typ';
 
 $GLOBALS['TL_LANG']['bbit_og']['type']
 	= array('Typ', 'Typ des auszuzeichnenden Gegenstands.');
